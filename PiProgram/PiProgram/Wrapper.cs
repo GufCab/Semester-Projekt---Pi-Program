c67d3921cef2b90c9b771b28facbaf1efc775019
Nikolaj Gustafsson <nikolaj@donald.dk>
2013-04-01 17:57:22 +0200

    Codings

--- PiProgram/PiProgram/Wrapper.cs
+++ PiProgram/PiProgram/Wrapper.cs
@@ -10,6 +10,10 @@ namespace PiProgram
 {
     public class Wrapper : IWrapper
     {
+        //Dette er selve de streams der skal skrives og læses fra!
+        private StreamWriter _inStream;
+        private StreamReader _outStream;
+        
         public Wrapper(string path)
         {
             var mplayer = new Process();
@@ -29,22 +33,24 @@ namespace PiProgram
             mplayer.StartInfo = startInfo;
             mplayer.Start();
 
-            
+            _inStream = mplayer.StandardInput;
+            _outStream = mplayer.StandardOutput;
+
         }
         
         public StreamReader GetOutputStream()
         {
-            
+            return null;
         }
 
         public int GetPos()
         {
-            
+            return 0;
         }
 
         public int GetTimeLeft()
         {
-            
+            return 0;
         }
 
         public void PauseTrack()
