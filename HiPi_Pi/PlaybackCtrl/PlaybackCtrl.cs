605ed203b90dace00275979ad7a89c8030b242f5
Casper Hougaard Jensen <casperhgrd@gmail.com>
2013-04-19 12:34:56 +0200

    Added PlaybackCtrl.cs

---
+++ HiPi_Pi/PlaybackCtrl/PlaybackCtrl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MPlayer;
+
+namespace Playback
+{
+    public class PlaybackCtrl
+    {
+        private IWrapper MPlayer;
+
+        private List<string> NextFiveTracks; 
+
+        //Constructor:
+        public PlaybackCtrl()
+        {
+            MPlayer = new Wrapper();
+            NextFiveTracks = new List<string>();
+
+        }
+        
+        public void Play()
+        {
+            if (NextFiveTracks.Count > 0)
+            {
+                MPlayer.PlayTrack(NextFiveTracks[0]);
+            }
+        }
+
+        public void AddURI(string path)
+        {
+            NextFiveTracks.Add(path);
+        }
+    }
+}
