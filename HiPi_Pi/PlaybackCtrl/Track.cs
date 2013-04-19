0b73a2b348b65d910722058bc01cb597fd229521
Casper Hougaard Jensen <casperhgrd@gmail.com>
2013-04-19 14:26:25 +0200

    Edited track

--- HiPi_Pi/PlaybackCtrl/Track.cs
+++ HiPi_Pi/PlaybackCtrl/Track.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +20,7 @@ namespace PlaybackCtrl
         public string Name;
         public int Duration;
         public string path;
+        DecoderFallback
 
 
 
