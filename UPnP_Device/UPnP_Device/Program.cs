837c2e05a2af6c015e55c39218b5bbda4727e6cb
Casper Hougaard Jensen <casperhgrd@gmail.com>
2013-05-02 16:34:40 +0200

    Added new solution - UPnP_Device - Created classes

---
+++ UPnP_Device/UPnP_Device/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UPnP_Device
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
