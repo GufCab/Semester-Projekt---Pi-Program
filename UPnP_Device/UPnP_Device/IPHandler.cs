da1003a049727c86703d77740eee9360d7c64e36
Nikolaj Gustafsson <nikolaj@donald.dk>
2013-05-02 17:35:03 +0200

    UPnP_Device/IPHandler.cs

---
+++ UPnP_Device/UPnP_Device/IPHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace UPnP_Device
+{
+    public static class IPHandler
+    {
+        public static string GetGUID()
+        {
+            var g = Guid.NewGuid();
+            return g.ToString();
+        }
+
+        public static string GetOwnIp()
+        {
+            //finds my ip address
+            string localIP = "";
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return localIP;
+        }
+    }
+}
