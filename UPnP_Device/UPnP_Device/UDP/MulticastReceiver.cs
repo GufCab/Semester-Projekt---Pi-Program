e413843ffe51aed5c03d34eb99dbc1bc3a250324
Casper Hougaard Jensen <casperhgrd@gmail.com>
2013-05-03 12:54:40 +0200

    Reception of M-SEARCH on multicast should work now. Not yet testet

--- UPnP_Device/UPnP_Device/UDP/MulticastReceiver.cs
+++ UPnP_Device/UPnP_Device/UDP/MulticastReceiver.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace UPnP_Device.UDP
 {
@@ -23,6 +24,8 @@ namespace UPnP_Device.UDP
         private static IPEndPoint recIPep;
         private static Byte[] recBuffer;
 
+         
+
         //Contructor:
         public MulticastReceiver(string uuid, int cacheexpire, string localip, int tcpport)
         {
@@ -31,12 +34,9 @@ namespace UPnP_Device.UDP
             _cacheexpire = cacheexpire;
             _localip = localip;
             _tcpport = tcpport;   
- 
-
         }
 
-
-        public static void SetupMulticastReceiver()
+        private static void SetupMulticastReceiver()
         {
             recClient = new UdpClient();
 
@@ -47,13 +47,14 @@ namespace UPnP_Device.UDP
         }
 
         //Blocks until reception
-        public static string ReceiveMulticast()
+        public string ReceiveMulticast()
         {
             Byte[] data = recClient.Receive(ref recIPep);
             string strData = Encoding.UTF8.GetString(data);
 
             return strData;
         }
+
 
     }
 }
--- UPnP_Device/UPnP_Device/UDP/UDPServer.cs
+++ UPnP_Device/UPnP_Device/UDP/UDPServer.cs
@@ -24,7 +24,8 @@ namespace UPnP_Device.UDP
         private Thread NotifyThread;
         private Thread ReceiveThread;
 
-        
+        private List<Thread> threadPool = new List<Thread>();
+
         public UDPServer(string uuid, int cacheexpire, string localip, int tcpport)
         {
             _UUID = uuid;
@@ -33,15 +34,49 @@ namespace UPnP_Device.UDP
             _tcpport = tcpport;
 
             sender = new MulticastSender(_UUID, _cacheexpire, _localip, _tcpport);
-
+            receiver = new MulticastReceiver(_UUID, _cacheexpire, _localip, _tcpport);
 
             NotifyThread = new Thread(sender.NotifySender);
+            ReceiveThread = new Thread(Run);
         }
 
         public void Start()
         {
             NotifyThread.Start();
+            ReceiveThread.Start();
         }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string msg = receiver.ReceiveMulticast();
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Handler), msg);
+                
+                /*
+                threadPool.Add(new Thread(() => Handler(msg)));     //Explained here: http://goo.gl/6uAgD
+                threadPool[(threadPool.Count-1)].Start();
+                */
+            }
+        }
+
+        public void Handler(object obj)
+        {
+            string msg = (string) obj;
+            //Splitting by string explained here: http://goo.gl/PSdtL
+            String[] splitter = new string[] { "\r\n" };
+            String[] msgArray = msg.Split(splitter, StringSplitOptions.None);   //Using overload method of split to take string array
+
+            if (msgArray[0] == "M-SEARCH * HTTP/1.1")
+            {
+                Console.WriteLine("This is right");
+            }
+            else
+            {
+                Console.WriteLine("Unknown input");
+            }
+        }
+
 
     }
 }
