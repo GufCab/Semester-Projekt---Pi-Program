8c06835a6e4871f7566903a6b870ac2a77c16adc
Christian Svejstrup <chrmaui@gmail.com>
2013-05-07 09:29:42 +0200

    xmlreader added upnp_device

---
+++ UPnP_Device/UPnP_Device/XMLReader1.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace UPnP_Device
+{
+    public class XMLReader1
+    {
+        public string loadAVTransportXML()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"\XML_files");
+
+            return doc.OuterXml;
+        }
+    }
+}
