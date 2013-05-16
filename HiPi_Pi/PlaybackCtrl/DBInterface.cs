e30e97ea8f7be2e1d44b2577263585b6900518f5
Aik Marco Makinen Petersen <amm.petersen@hotmail.com>
2013-05-16 14:15:54 +0200

    Work In Progress

--- HiPi_Pi/PlaybackCtrl/DBInterface.cs
+++ HiPi_Pi/PlaybackCtrl/DBInterface.cs
@@ -9,12 +9,15 @@ namespace PlaybackCtrl
     {
         //Skal returnere MetaData - Konverteres til ITrack efterfølgende
         ITrack GetTrack(int index);
-        void AddToPlayQueue();
-        void AddToPlayQueue(int index);
+        void AddToPlayQueue(string s);
+        void AddToPlayQueue(string s, int index);
+        int GetNumberOfTracks();
     }
 
     class PlayQueueDB : IPlayQueueDB
     {
+        private int _numberOfTracks = 0;
+
         //Returnerer MetaData fra tracket på plads "index" i PlayQueuen
         public ITrack GetTrack(int index)
         {
@@ -22,13 +25,19 @@ namespace PlaybackCtrl
         }
 
         //Tilføjer til bunden af PlayeQueue
-        public void AddToPlayQueue()
+        public void AddToPlayQueue(string s)
         {
             throw new NotImplementedException();
         }
 
         //Tilføjer på plads "index" i PlayQueue
-        public void AddToPlayQueue(int index)
+        public void AddToPlayQueue(string s, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Ser om listen er tom
+        public int GetNumberOfTracks()
         {
             throw new NotImplementedException();
         }
