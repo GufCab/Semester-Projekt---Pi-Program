3de24fbf42be2f773f2fb0e7e26fd9855831c8d4
Aik Marco Makinen Petersen <amm.petersen@hotmail.com>
2013-05-16 13:25:54 +0200

    DBInterface added

---
+++ HiPi_Pi/PlaybackCtrl/DBInterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlaybackCtrl
+{
+    public interface IPlayQueueDB
+    {
+        ITrack GetTrack(int index);
+        void AddToPlayQueue();
+        void AddToPlayQueue(int index);
+    }
+
+    class PlayQueueDB : IPlayQueueDB
+    {
+        //Returnerer MetaData fra tracket på plads "index" i PlayQueuen
+        public ITrack GetTrack(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Tilføjer til bunden af PlayeQueue
+        public void AddToPlayQueue()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Tilføjer på plads "index" i PlayQueue
+        public void AddToPlayQueue(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
