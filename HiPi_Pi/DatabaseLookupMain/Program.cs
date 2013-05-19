da927be8a8619f1b673d687d5ce10252c6f35894
Casper Hougaard Jensen <casperhgrd@gmail.com>
2013-05-19 18:10:34 +0200

    Implementation of DbLookup.cs

---
+++ HiPi_Pi/DatabaseLookupMain/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Containers;
+using DbLookup;
+
+namespace DatabaseLookupMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new DbLookup.DbLookup();
+
+            var trkList = data.Browse("hej");
+
+            foreach (ITrack t in trkList)
+            {
+                Console.WriteLine("---- " + t.FileName + "----");
+                Console.WriteLine("Track Title: " + t.Title);
+                Console.WriteLine("Track DeviceIP: " + t.DeviceIP);
+                Console.WriteLine("Track Artist: " + t.Artist);
+                Console.WriteLine("Track Album: " + t.Album);
+                Console.WriteLine("Track Duration: " + t.Duration);
+                Console.WriteLine("Track Genre: " + t.Genre);
+                Console.WriteLine("Track Protocol: " + t.Protocol);
+                Console.WriteLine("Track Path: " + t.Path);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
